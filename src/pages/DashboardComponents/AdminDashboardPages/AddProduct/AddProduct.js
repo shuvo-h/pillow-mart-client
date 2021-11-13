6ccbfd4f39ccffd60de0144b7e8d7c5ecb9a3235
shuvo-h <shuvowriter@gmail.com>
2021-11-13 18:01:53 +0600

    more responsive changed

--- src/pages/DashboardComponents/AdminDashboardPages/AddProduct/AddProduct.js
+++ src/pages/DashboardComponents/AdminDashboardPages/AddProduct/AddProduct.js
@@ -38,17 +38,17 @@ const AddProduct = () => {
             </Typography>
             <Box sx={{p:2, borderRadius:"10px", width:"fit-content", boxShadow:"2px 2px 10px 1px Cyan", backgroundColor:"Lavender"}}>
                 <form sx={{display:"flex", flexDirection: 'column' }} onSubmit={handleAddProduct}> 
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="title" type="text" id="title" label="Product Name" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="price" type="number" id="price" label="Price in Tk." variant="standard" required /></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="color"  type="text" id="color" label="Color" variant="standard" required /></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="size" type="text" id="size" label="Size (height x width)" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="size_metric" type="text" id="size_metric" label="Size Metric (cm/inches)" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="pattern" type="text" id="pattern" label="Pattern" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="Fabric" type="text" id="Fabric" label="Fabric Type" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="shape" type="text" id="shape" label="Shape" variant="standard" required/></Box>
-                    <Box sx={{my:1}}><TextField sx={{width:"350px"}} onBlur={handleOnBlur} name="photoUrl" type="text" id="photoUrl" label="Image url" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="title" type="text" id="title" label="Product Name" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="price" type="number" id="price" label="Price in Tk." variant="standard" required /></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="color"  type="text" id="color" label="Color" variant="standard" required /></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="size" type="text" id="size" label="Size (height x width)" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="size_metric" type="text" id="size_metric" label="Size Metric (cm/inches)" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="pattern" type="text" id="pattern" label="Pattern" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="Fabric" type="text" id="Fabric" label="Fabric Type" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="shape" type="text" id="shape" label="Shape" variant="standard" required/></Box>
+                    <Box sx={{my:1}}><TextField sx={{width:{xs:"250px",sm:"350px"}}} onBlur={handleOnBlur} name="photoUrl" type="text" id="photoUrl" label="Image url" variant="standard" required/></Box>
                     <Box sx={{my:1}}><TextareaAutosize style={{width:"100%", backgroundColor:"Lavender", fontSize:"18px"}} onBlur={handleOnBlur} name="str" type="text" id="str" label="Description" variant="standard" placeholder="Product description" minRows={4} required/></Box>
-                    <Button sx={{my:3}} type="submit" variant="contained">Create an account</Button>
+                    <Button sx={{my:3}} type="submit" variant="contained">Add Product</Button>
                 </form>
             </Box>
         </Box>
